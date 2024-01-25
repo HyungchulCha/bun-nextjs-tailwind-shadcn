3a4ba6db250e969a5c173d3503033d33b6d2cf4e
HyungchulCha <ckgudcjf@gmail.com>
2024-01-25 21:55:21 +0900

    modify:localfont, metadata, viewport, delete:asset, move:public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <div className='text-24pxr'>가나다라마바사아자차카타파하 1234567890</div>
+    </div>
+  );
 }
---
+++ src/style/localfont.ts
@@ -0,0 +1,95 @@
+import localFont from 'next/font/local';
+export const localfont = localFont({
+  src: [
+    {
+      path: '../asset/font/Pretendard-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-ExtraLight.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Black.woff',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-ExtraBold.woff',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-SemiBold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Light.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-ExtraLight.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../asset/font/Pretendard-Thin.woff',
+      weight: '100',
+      style: 'normal',
+    },
+  ],
+});
